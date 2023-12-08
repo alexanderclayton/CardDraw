fc0f9b4dc374a29a6c33b632e5a5bc34a994a52f
Alex Clayton <alexander.clayton.email@gmail.com>
2023-12-08 11:35:49 -0600

    setup routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 //import//
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Game } from "./pages/Game";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
