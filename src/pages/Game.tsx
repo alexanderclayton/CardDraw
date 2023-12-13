31b806e4ca77075fe780a86fe8e7c6bc2c9c89d4
Alex Clayton <alexander.clayton.email@gmail.com>
2023-12-13 11:23:58 -0600

    use location to pass time limits into game

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Countdown } from "../components/Countdown";
 
 export const Game: React.FC = () => {
+  const location = useLocation();
+  const time = location.state;
+
+  console.log(time);
+
+  if (time == undefined) {
+    return <p>No Time Limit!</p>;
+  }
+
   return (
     <div>
-      <Countdown initialSeconds={3} />
+      <Countdown initialSeconds={time} />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import Logo from "../assets/creatureslogo.jpg";
 export const Home: React.FC = () => {
   const navigate = useNavigate();
 
-  const toGame = () => {
-    navigate("/game");
+  const toGame = (time: number | null) => {
+    navigate("/game", { state: time });
   };
 
   return (
@@ -19,14 +19,20 @@ export const Home: React.FC = () => {
         <div className="flex justify-around pt-8">
           <div
             className="bg-red-500 rounded-lg w-[20%] h-36 flex justify-center items-center hover:cursor-pointer hover:bg-red-700"
-            onClick={toGame}
+            onClick={() => toGame(300)}
           >
             <p className="font-bold text-3xl">5 Minutes</p>
           </div>
-          <div className="bg-red-500 rounded-lg w-[20%] h-36 flex justify-center items-center hover:cursor-pointer hover:bg-red-700">
+          <div
+            className="bg-red-500 rounded-lg w-[20%] h-36 flex justify-center items-center hover:cursor-pointer hover:bg-red-700"
+            onClick={() => toGame(900)}
+          >
             <p className="font-bold text-3xl">15 Minutes</p>
           </div>
-          <div className="bg-red-500 rounded-lg w-[20%] h-36 flex justify-center items-center hover:cursor-pointer hover:bg-red-700">
+          <div
+            className="bg-red-500 rounded-lg w-[20%] h-36 flex justify-center items-center hover:cursor-pointer hover:bg-red-700"
+            onClick={() => toGame(null)}
+          >
             <p className="font-bold text-3xl">Open Play</p>
           </div>
         </div>
