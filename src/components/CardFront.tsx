8e94444644b778df15a106146fdfe00f410f3f67
Alex Clayton <alexander.clayton.email@gmail.com>
2023-12-14 13:26:16 -0600

    pull random card image from firebase

--- src/components/CardFront.tsx
+++ src/components/CardFront.tsx
@@ -1,54 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase/firebaseConfig";
+import { collection, getDocs, query } from "firebase/firestore";
+
 import SmallFront from "../assets/cardTypes/smallFront.JPG";
 import MediumFront from "../assets/cardTypes/mediumFront.JPG";
 import LargeFront from "../assets/cardTypes/largeFront.JPG";
 import HabitatFront from "../assets/cardTypes/habitatFront.JPG";
 import DetailFront from "../assets/cardTypes/detailFront.JPG";
+import { FirebaseError } from "firebase/app";
 
 interface CardFrontProps {
   cardType: string;
 }
 
 export const CardFront: React.FC<CardFrontProps> = ({ cardType }) => {
-  if (cardType === "small")
-    return (
-      <img
-        src={SmallFront}
-        alt="small card front"
-        className="w-48 h-72 object-cover rounded-xl m-4 border-black border"
-      />
-    );
-  if (cardType === "medium")
-    return (
-      <img
-        src={MediumFront}
-        alt="medium card front"
-        border-black
-        className="w-48 h-72 object-cover rounded-xl m-4 border-black border"
-      />
-    );
-  if (cardType === "large")
-    return (
-      <img
-        src={LargeFront}
-        alt="large card front"
-        className="w-48 h-72 object-cover rounded-xl m-4 border-black border"
-      />
-    );
-  if (cardType === "habitat")
-    return (
-      <img
-        src={HabitatFront}
-        alt="habitat card front"
-        className="w-48 h-72 object-cover rounded-xl m-4 border-black border"
-      />
-    );
-  if (cardType === "detail")
-    return (
-      <img
-        src={DetailFront}
-        alt="detail card front"
-        className="w-48 h-72 object-cover rounded-xl m-4 border-black border"
-      />
-    );
+  const [cardImage, setCardImage] = useState<string>("");
+
+  useEffect(() => {
+    const fetchRandomCardImageUrl = async () => {
+      try {
+        const cardsCollection = collection(db, cardType);
+        const cardsQuery = query(cardsCollection);
+        const cardsSnapshot = await getDocs(cardsQuery);
+
+        if (!cardsSnapshot.empty) {
+          // Get a random index
+          const randomIndex = Math.floor(Math.random() * cardsSnapshot.size);
+
+          // Get the document data at the random index
+          const randomCardData = cardsSnapshot.docs[randomIndex].data();
+          setCardImage(randomCardData.imageUrl);
+        } else {
+          console.error("No cards found in the collection");
+        }
+      } catch (error) {
+        console.error(
+          "Error fetching random card image URL:",
+          (error as FirebaseError).message
+        );
+      }
+    };
+
+    fetchRandomCardImageUrl();
+  }, []);
+
+  if (!cardImage) {
+    // Handle loading state or display a placeholder **Add Static Wildcard Here
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <img
+      src={cardImage}
+      alt="small card front"
+      className="w-48 h-72 object-cover rounded-xl m-4 border-black border"
+    />
+  );
 };
