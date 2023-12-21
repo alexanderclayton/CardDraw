33cdf214f91dd21ef6ed7b8b4581cb84382a6731
Alex <alexander.clayton.email@gmail.com>
2023-12-21 09:12:22 -0600

    added howto page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Game } from "./pages/Game";
 import { ConfigureGame } from "./pages/ConfigureGame";
+import { HowTo } from "./pages/HowTo";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
         <Route path="/configure" element={<ConfigureGame />} />
+        <Route path="/howto" element={<HowTo />} />
       </Routes>
     </Router>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
   return (
     <nav>
       <Link to="/configure">Configure Game</Link>
+      <Link to="/howto">How To Play</Link>
     </nav>
   );
 };
